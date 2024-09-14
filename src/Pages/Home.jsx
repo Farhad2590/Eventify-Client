1f797bcef3e099ff317676aa880ff421790dbbd5
Marufhossain07 <marufprogrammer07@gmail.com>
2024-09-14 18:37:56 +0600

    our events and cta added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,12 @@
+import CallToAction from "./home sections/CallToAction";
+import OurEvents from "./home sections/OurEvents";
 
 const Home = () => {
     return (
         <div>
             Hello From Home
+            <OurEvents></OurEvents>
+            <CallToAction></CallToAction>
         </div>
     );
 };
---
+++ src/Pages/home sections/OurEvents.jsx
@@ -0,0 +1,78 @@
+import concert from '../../assets/concerts.jfif'
+import birthday from '../../assets/birthday.jfif'
+import conference from '../../assets/conference.jfif';
+import wedding from '../../assets/wedding.jpg'
+import festival from '../../assets/festivals.jfif'
+import newYear from '../../assets/new-year.jfif'
+
+const OurEvents = () => {
+    return (
+        <div className='container mx-auto'>
+            <h3 className="text-center text-5xl font-semibold mb-10">Our Events</h3>
+            <div  className='grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+            <div>
+                <div className=" rounded-md shadow-md">
+                    <img src={concert} alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
+                    <div className="bg-[#3A86FF] p-2 rounded-b">
+                        <div>
+                            <h2 className="text-3xl font-semibold text-white text-center  tracking-wide">Concert</h2>
+                            </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div className=" rounded-md shadow-md">
+                    <img src={birthday} alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
+                    <div className="bg-[#3A86FF] p-2 rounded-b">
+                        <div>
+                            <h2 className="text-3xl font-semibold text-white text-center  tracking-wide">Birthday</h2>
+                            </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div className=" rounded-md shadow-md">
+                    <img src={festival} alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
+                    <div className="bg-[#3A86FF] p-2 rounded-b">
+                        <div>
+                            <h2 className="text-3xl font-semibold text-white text-center  tracking-wide">Festival</h2>
+                            </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div className=" rounded-md shadow-md">
+                    <img src={wedding} alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
+                    <div className="bg-[#3A86FF] p-2 rounded-b">
+                        <div>
+                            <h2 className="text-3xl font-semibold text-white text-center  tracking-wide">Wedding</h2>
+                            </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div className=" rounded-md shadow-md">
+                    <img src={newYear} alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
+                    <div className="bg-[#3A86FF] p-2 rounded-b">
+                        <div>
+                            <h2 className="text-3xl font-semibold text-white text-center  tracking-wide">New Year Party</h2>
+                            </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div className=" rounded-md shadow-md">
+                    <img src={conference} alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
+                    <div className="bg-[#3A86FF] p-2 rounded-b">
+                        <div>
+                            <h2 className="text-3xl font-semibold text-white text-center  tracking-wide">Conference</h2>
+                            </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    );
+};
+
+export default OurEvents;
