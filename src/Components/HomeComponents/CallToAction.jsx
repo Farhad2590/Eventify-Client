87a7280755a96e6508aaf0b01a837fa4f84836d4
Farhad2590 <farhadhossen2590@gmail.com>
2024-09-15 16:54:12 +0600

    Development Branch

--- src/Components/HomeComponents/CallToAction.jsx
+++ src/Components/HomeComponents/CallToAction.jsx
@@ -5,12 +5,12 @@ const CallToAction = () => {
             <div className="space-y-5">
                 <h3 className="text-7xl text-black font-medium">Planning for something ?</h3>
                 <p className="text-4xl text-black">Let us complete it for you</p>
-                <button className="btn bg-[#3A86FF] flex-nowrap px-7 py-5 text-white border-none text-lg">Book Now</button>
+                <button className="btn bg-pink-400 flex-nowrap px-7 py-5 text-white border-none text-lg">Book Now</button>
             </div>
             <div className="flex  justify-between gap-10">
-                <img className="h-60 w-60 rounded-full border shadow-[#3A86FF] shadow-md  cta1" alt="" />
-                <img className="h-60 w-60 rounded-full border shadow-[#3A86FF] shadow-md  cta2" alt="" />
-                <img className="h-60 w-60 rounded-full border shadow-[#3A86FF] shadow-md  cta3" alt="" />
+                <img className="h-60 w-60 rounded-full border shadow-pink-400 shadow-md  cta1" alt="" />
+                <img className="h-60 w-60 rounded-full border shadow-pink-400 shadow-md  cta2" alt="" />
+                <img className="h-60 w-60 rounded-full border shadow-pink-400 shadow-md  cta3" alt="" />
 
             </div>
         </div>
