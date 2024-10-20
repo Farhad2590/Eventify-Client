a43af165aaad349f197defa9d7344c9c3ac21a08
Marufhossain07 <marufprogrammer07@gmail.com>
2024-10-21 01:54:12 +0600

    testimonial slider added

--- src/Components/HomeComponents/Testimonial.jsx
+++ src/Components/HomeComponents/Testimonial.jsx
@@ -1,46 +1,69 @@
+import { useEffect, useState } from "react";
 import TitleAndSubheading from "../../Shared/TitleAndSubheading";
+import useAxiosSecure from "../../hooks/useAxiosSecure"
+import TestimonialCard from "./TestimonialCard";
 
-const TestimonialCard = ({ image, name, role, content }) => (
-	<div className="bg-gray-100 p-6 rounded-lg shadow-md flex flex-col items-center">
-	  <img src={image} alt={name} className="w-20 h-20 rounded-full mb-4" />
-	  <p className="text-gray-700 text-center mb-4">{content}</p>
-	  <p className="font-bold text-blue-600">{name}</p>
-	  <p className="text-sm text-gray-600">{role}</p>
-	</div>
-  );
-  
-  const Testimonial = () => {
-	const testimonials = [
-	  {
-		image: "https://i.ibb.co/ScLz5b5/pic1.jpg",
-		name: "Avinash Kr",
-		role: "Co-Founder at xyz",
-		content: "Like this vide and ask your questions in comment section, don't forget to Subscribe Easy Tutorials YouTube channel to watch more videos of website designing, digital marketing and photoshop."
-	  },
-	  {
-		image: "https://i.ibb.co/cT2y4cB/pic2.jpg",
-		name: "Bharat Kunal",
-		role: "Manager at xyz",
-		content: "Like this vide and ask your questions in comment section, don't forget to Subscribe Easy Tutorials YouTube channel to watch more videos of website designing, digital marketing and photoshop."
-	  },
-	  {
-		image: "https://i.ibb.co/8gbSZKg/pic3.jpg",
-		name: "Prabhakar D",
-		role: "Founder / CEO at xyz",
-		content: "Like this vide and ask your questions in comment section, don't forget to Subscribe Easy Tutorials YouTube channel to watch more videos of website designing, digital marketing and photoshop."
-	  }
-	];
-  
+import { SwiperSlide, Swiper } from "swiper/react";
+import { Pagination } from "swiper/modules";
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import '../../index.css';
+
+
+const Testimonial = () => {
+	const [reviews, setReviews] = useState([])
+	const axiosSecure = useAxiosSecure()
+
+
+	useEffect(() => {
+		console.log("useEffect is triggered");
+		axiosSecure('/reviews')
+			.then(res => {
+				setReviews(res?.data)
+			})
+			.catch(err => {
+				console.log(err);
+
+			})
+	}, [axiosSecure])
+
 	return (
-	  <div className="container mx-auto px-4 py-8">
-		<TitleAndSubheading title="TESTIMONIALS"></TitleAndSubheading>
-		<div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-		  {testimonials.map((testimonial, index) => (
-			<TestimonialCard key={index} {...testimonial} />
-		  ))}
-		</div>
-	  </div>
+		<div className="container mx-auto px-4 py-8">
+			<TitleAndSubheading title="TESTIMONIALS"></TitleAndSubheading>
+			<Swiper
+			            breakpoints={{
+							'@0.00': {
+							  slidesPerView: 1,
+							  spaceBetween: 10,
+							},
+							'@0.75': {
+							  slidesPerView: 1,
+							  spaceBetween: 10,
+							},
+							'@1.00': {
+							  slidesPerView: 3,
+							  spaceBetween: 10,
+							},
+							'@1.50': {
+							  slidesPerView: 3,
+							  spaceBetween: 20,
+							},
+						  }}
+				slidesPerView={3}
+				pagination={{
+					clickable: true,
+				}}
+				modules={[Pagination]}
+				className="mySwiper"
+			>
+				{reviews.map((review, index) => (
+					<SwiperSlide><TestimonialCard key={index} {...review} /></SwiperSlide>
+				))}
+				
+			</Swiper>
+		</div >
 	);
-  };
-  
-  export default Testimonial;
+};
+
+export default Testimonial;
